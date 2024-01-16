38adec235c09668bfbe25f2e0e1fad3fd3e2b82d
bno4 <bnduchemin@gmail.com>
2024-01-16 10:45:04 +0100

    window.addEvent... sur le scrollY

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const links = document.querySelectorAll("nav li");
 console.log(links);
 
+const nav = document.querySelector("nav");
+console.log(nav);
+let lastScrollPosition = 0;
+
 icons.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
@@ -9,4 +13,14 @@ links.forEach((link) => {
   link.addEventListener("click", () => {
     nav.classList.remove("active");
   });
+});
+
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > lastScrollPosition) {
+    nav.style.top = "-60px";
+    nav.style.opacity = "0";
+  } else {
+    nav.style.top = "0px";
+    nav.style.opacity = "1";
+  }
 });
